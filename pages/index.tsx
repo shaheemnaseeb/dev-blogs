02fa5465cbe3d6ff75876964997d8217cc16e1d4
shaheemnaseeb <shaheemnaseeb@gmail.com>
2024-06-02 05:05:24 +0200

    removed unnecessary file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold text-center">Hello World</h1>
+    </div>
+  );
 }
